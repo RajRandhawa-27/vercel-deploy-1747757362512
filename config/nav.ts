3635a526723a55c9f03636ce441c78f33570d41a
RajRandhawa-27 <richrandhawa03@gmail.com>
2025-05-20 21:40:16 +0530

    Add config/nav.ts

---
+++ config/nav.ts
@@ -0,0 +1,26 @@
+export const navItems = [
+  {
+    label: "Home",
+    href: "#hero",
+  },
+  {
+    label: "Expertise",
+    href: "#expertise",
+  },
+  {
+    label: "Work",
+    href: "#work",
+  },
+  {
+    label: "Services",
+    href: "#services",
+  },
+  {
+    label: "Process",
+    href: "#process",
+  },
+  {
+    label: "Contact",
+    href: "#contact",
+  },
+]
