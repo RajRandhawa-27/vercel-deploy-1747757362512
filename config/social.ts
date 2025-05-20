532b6a17d523c13f3548ae2b6c52c270779f23bc
RajRandhawa-27 <richrandhawa03@gmail.com>
2025-05-20 21:40:17 +0530

    Add config/social.ts

---
+++ config/social.ts
@@ -0,0 +1,34 @@
+import { Github, Linkedin, Twitter, Instagram, Dribbble, Youtube } from "lucide-react"
+
+export const socialLinks = [
+  {
+    name: "GitHub",
+    url: "https://github.com/johndoe",
+    icon: Github,
+  },
+  {
+    name: "LinkedIn",
+    url: "https://linkedin.com/in/johndoe",
+    icon: Linkedin,
+  },
+  {
+    name: "Twitter",
+    url: "https://twitter.com/johndoe",
+    icon: Twitter,
+  },
+  {
+    name: "Instagram",
+    url: "https://instagram.com/johndoe",
+    icon: Instagram,
+  },
+  {
+    name: "Dribbble",
+    url: "https://dribbble.com/johndoe",
+    icon: Dribbble,
+  },
+  {
+    name: "YouTube",
+    url: "https://youtube.com/johndoe",
+    icon: Youtube,
+  },
+]
