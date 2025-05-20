b2708d9b5a94c4a0d85167c78819b9c41ba618c4
RajRandhawa-27 <richrandhawa03@gmail.com>
2025-05-20 21:40:19 +0530

    Add data/personalData.ts

---
+++ data/personalData.ts
@@ -0,0 +1,14 @@
+export const personalData = {
+  "firstName": "Karandeep",
+  "lastName": "Randhawa",
+  "title": "kbkghkj",
+  "greeting": "khkbkjb",
+  "status": "kbkbkjh",
+  "summary": "",
+  "profileImage": "",
+  "resumeUrl": "",
+  "email": "",
+  "phone": "",
+  "telegram": "",
+  "location": ""
+};
